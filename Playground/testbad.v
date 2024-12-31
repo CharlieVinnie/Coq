2e4e8655abad925c4b9fb9439609d0845129274b
Charlie <charliewang.cpp@gmail.com>
2024-12-31 16:38:00 +0800

    fix exfalso; lia

--- Playground/testbad.v
+++ Playground/testbad.v
@@ -1,11 +1,12 @@
 From CRUSH Require Import Crush.
 Require Import Arith.
 
-Goal forall n, (n<=0 <-> n<=1) -> S n <= 2.
+Goal forall (P Q: nat -> Prop) m, (P m <-> Q m) -> Q m.
+Proof.
   Crush.
 Abort.
 
 Goal forall n m, ( (n <=? m) = false <-> n <= m ) -> n <= m.
 Proof.
   intros. destruct H as [H1 H2]. 
-  crush.
+  Crush.
