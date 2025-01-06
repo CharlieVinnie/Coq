de0587fdc79597ba11d37c75a739e6fe6e26e470
Charlie <charliewang.cpp@gmail.com>
2025-01-06 17:44:21 +0800

    list_analyze created

--- Playground/list_analyze.v
+++ Playground/list_analyze.v
@@ -16,75 +16,50 @@ Local Lemma list_app_2 : forall (T:Type) (a b c d e:list T),
   a++(b++c)++d++e = a++(b++c++d)++e.
 Proof. intros;repeat rewrite app_assoc;reflexivity. Qed.
 
-Ltac list_splitter' tac :=
-  match goal with
-      | [ |- context[ ?o ++ ?a ++ ?b ++ ?c ] ] => rewrite list_app_1
-      | [ |- context[ ?o ++ (?a ++ ?b) ++ ?c ++ ?d ] ] => rewrite list_app_2
-      | [ |- context[ ?o ++ ?a ++ ?b ] ] =>
-        let a' := fresh "a" in
-          remember a as a'; rewrite app_assoc
-  end;try tac;list_splitter' tac.
+Print app_assoc.
 
-Ltac list_splitter tac :=
-  match goal with
-      (* | [ |- context[ ?o ++ ?a ++ ?b ++ ?c ] ] => rewrite list_app_1
-      | [ |- context[ ?o ++ (?a ++ ?b) ++ ?c ++ ?d ] ] => rewrite list_app_2 *)
-      | [ |- context[ ?o ++ ?a ++ ?b ] ] =>
-        let a' := fresh "a" in
-          remember a as a'; rewrite app_assoc
-  end;try tac.
-
-Goal forall T (x1 x2 x3: list T),
-  F x1 -> F (x2++x3) -> F (x1++x2++x3).
-Proof.
-  intros. rewrite <- app_nil_l.
-  (* match goal with
-      (* | [ |- context[ ?o ++ ?a ++ ?b ++ ?c ] ] => rewrite list_app_1 *)
-      (* | [ |- context[ ?o ++ (?a ++ ?b) ++ ?c ++ ?d ] ] => rewrite list_app_2 *)
-      | [ |- context[ ?o ++ ?a ++ ?b ] ] =>
-        let a' := fresh "a" in
-          remember a as a'; rewrite app_assoc
+Ltac get_sub_helper l cont :=
+  match l with
+  | ?a ++ ?b =>
+    match goal with
+    | [ |- context [a] ] =>
+      get_sub_helper b ltac:(
+        match goal with
+        | [ |- context [ a ++ b ++ ?c ] ] => rewrite (app_assoc a b c)
+        end;cont
+      )
+    end
+  | _ =>
+    match goal with
+    | [ |- context [l] ] => cont
+    end
   end.
-  solve[crush]. *)
-  list_splitter ltac:(solve[crush]).
 
-Goal forall T (x1 x2 x3 x4 x5 x6 x7 l1 l2 l3 l4 l5: list T),
+Ltac get_sub l := get_sub_helper l idtac.
+
+Ltac subst_sub l :=
+  let l' := fresh "l" in
+  let H := fresh "Heq_l" in
+  sets_eq l' H: l; clear H.
+
+Ltac generalize_sub l := get_sub l; subst_sub l.
+
+Ltac try_generalize_sublists solver :=
+  match goal with
+  | [ H: context [ ?a ++ ?b ] |- _ ] =>
+    generalize_sub (a++b); try solve[solver]; try_generalize_sublists solver
+  end.
+
+Goal forall T (x1 x2 x3 x4 x5 x6 x7 x8 l1 l2 l3 l4 l5: list T),
   l1 = x1 ++ x2 ->
   l2 = x3 ++ x4 ->
   l3 = x6 ++ x7 ->
   l4 = x1 ++ x2 ++ x3 ->
-  l5 = x4 ++ x5 ++ x6 ->
-  F l1 -> F l2 -> F x5 -> F l3 ->
-  F (x1++x2++x3++x4++x5++x6++x7).
+  l5 = x5 ++ x6 ++ x7 ->
+  (* F l1 -> F l2 -> F x5 -> F l3 -> *)
+  F l4 -> F x4 -> F l5 -> F x8 ->
+  F (x1++x2++x3++x4++x5++x6++x7++x8).
 Proof.
-  intros.
-  (* cuts_rewrite <- ( (x1++x2)++(x3++x4)++x5++(x6++x7) = x1++x2++x3++x4++x5++x6++x7 ).
-  remember (x1++x2) as a.
-  remember (x3++x4) as b.
-  remember (x5) as c.
-  remember (x6++x7) as d.
-  try subst. crush. *)
-  repeat rewrite <- app_assoc in *. simpl in *.
-  rewrite <- app_nil_l.
-  list_splitter (idtac "hey").
-  repeat (
-    match goal with
-      | [ |- context[ ?o ++ ?a ++ ?b ++ ?c ] ] => rewrite list_app_1
-      | [ |- context[ ?o ++ (?a ++ ?b) ++ ?c ++ ?d ] ] => rewrite list_app_2
-      | [ |- context[ ?o ++ ?a ++ ?b ] ] =>
-        let a' := fresh "a" in
-          remember a as a'; rewrite app_assoc
-      (* | [ H : ?a =~ _ |- context [?a ++ _] ] =>
-        let a' := fresh "a" in
-          sets_eq a': a
-      | [ |- context [?ab ++ ?cd] ] =>
-        match ab with
-        | ?a ++ ?b =>
-          match cd with
-          | ?c ++ ?d => rewrite list_app_progress
-          | _ => fail 3
-          end
-        | _ => first [ rewrite app_assoc | fail 2 ]
-        end *)
-    end;try solve[crush]
-  ).
+  intros. simpl. repeat rewrite <- app_assoc.
+  try_generalize_sublists crush.
+Qed.
--- Playground/test_subgoals.v
+++ Playground/test_subgoals.v
@@ -2,6 +2,7 @@ Ltac bad := bad.
 
 Goal True.
 Proof.
+  try constructor.
   (try constructor); match goal with | _ => idtac end; bad.
   (* first [(match goal with | _ => bad end)|idtac]. *)
 Qed.
