7469489a1f4d5206c2d3be8fa641784928e78038
Charlie <charliewang.cpp@gmail.com>
2025-01-18 12:47:33 +0800

    fixed crush.v to reintro hypotheses

--- Playground/test_intro_order.v
+++ Playground/test_intro_order.v
@@ -9,10 +9,14 @@ Ltac nats n tac :=
 Inductive obj (n:nat) :=
   | qwq.
 
-Goal forall (P:nat -> Prop), True.
+Goal forall (P:nat -> Prop) (a b c d :nat), True.
 Proof.
   intros.
   nats 10 ltac:(fun n=>let Q:=fresh "Q" in specialize (qwq n) as Q).
+  specialize (qwq a) as A.
+  generalize Q. clear Q. intro Q.
+  generalize A. clear A. intro A.
+
   repeat match goal with
   | [ H : _ |- _ ] => generalize H;clear H
   end.
